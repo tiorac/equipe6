fba648d372efdc3dfbc48c2ed200a0db6ad8ab1b
Tiago Goes Teles <tiago_teles@outlook.com>
2020-08-26 22:25:08 -0300

    mesa

--- Truco/Mesa.cs
+++ Truco/Mesa.cs
@@ -18,13 +18,38 @@ namespace Equipe6.Truco
             Baralho = new Baralho();
 
             Baralho.GerarBaralho();
-            Distribuir();
-            
+            Distribuir(true);
+
+            if (Jogador1.PontuacaoGeral < 12 && Jogador2.PontuacaoGeral < 12)
+            {
+                Jogador1.PontuacaoRodada = 0;
+                Jogador2.PontuacaoRodada = 0;
+
+                if (Jogador1.PontuacaoRodada < 2 && Jogador2.PontuacaoRodada < 2)
+                {
+                    Rodada();
+                }
+
+                if (Jogador1.PontuacaoRodada == 2)
+                {
+                    Jogador1.PontuacaoGeral++;
+                }
+                else if(Jogador2.PontuacaoRodada == 2)
+                {
+                    Jogador2.PontuacaoGeral++;
+                }
+
+                Distribuir(false);
+            }
+
         }
 
-        public void Distribuir()
+
+
+        public void Distribuir(bool virar)
         {
-            CartaVirada = Baralho.GetTopCard();
+            if(virar)
+                CartaVirada = Baralho.GetTopCard();
 
             Jogador1.Carta1 = Baralho.GetTopCard();
             Jogador1.Carta2 = Baralho.GetTopCard();
@@ -35,6 +60,16 @@ namespace Equipe6.Truco
             Jogador2.Carta3 = Baralho.GetTopCard();
         }
 
-
+        public void Rodada()
+        {
+            if (Jogador1.CartaSelecionada.Valor > Jogador2.CartaSelecionada.Valor)
+            {
+                Jogador1.PontuacaoRodada++;
+            }
+            else
+            {
+                Jogador2.PontuacaoRodada++;
+            }
+        }
     }
 }
--- Truco/Player.cs
+++ Truco/Player.cs
@@ -10,7 +10,30 @@ namespace Equipe6.Truco
         public Carta Carta2 { get; set; }
         public Carta Carta3 { get; set; }
 
+        public Carta CartaSelecionada { get; set; }
+
         public int PontuacaoRodada;
         public int PontuacaoGeral;
+
+        public void SelecionarCarta(Carta carta)
+        {
+            if (Carta1 == carta)
+            {
+                CartaSelecionada = carta;
+                Carta1 = null;
+            }
+
+            if (Carta2 == carta)
+            {
+                CartaSelecionada = carta;
+                Carta2 = null;
+            }
+
+            if (Carta3 == carta)
+            {
+                CartaSelecionada = carta;
+                Carta3 = null;
+            }
+        }
     }
 }
