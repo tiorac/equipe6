e0630951d08d22f18728ef98ecf4794dcd3cc0bb
Tiago Goes Teles <tiago_teles@outlook.com>
2020-08-26 21:50:23 -0300

    Truco

--- Truco/Baralho.cs
+++ Truco/Baralho.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Equipe6.Truco
 {
     class Baralho
     {
-        void GerarBaralho()
-        {
+        public List<Carta> Cartas = new List<Carta>();
 
+        public void GerarBaralho()
+        {
+            List<Carta> cartas = new List<Carta>();
+            
+            
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var carta = new Carta
+                    {
+                        naipe = (Naipes)j,
+                        valor = (ValorCarta)i
+                    };
+
+                    cartas.Add(carta);
+                }
+            }
+
+            Random r = new Random();
+            Cartas = cartas.OrderBy(x => r.Next()).ToList();
+        }
+
+        public Carta GetTopCard()
+        {
+            Carta c = Cartas[0];
+            Cartas.RemoveAt(0);
+
+            return c;
         }
     }
 }
--- Truco/Mesa.cs
+++ Truco/Mesa.cs
@@ -6,16 +6,33 @@ namespace Equipe6.Truco
 {
     public class Mesa
     {
-        Player Jogador1 = new Player();
-        Player Jogador2 = new Player();
+        Player Jogador1;
+        Player Jogador2;
+        Baralho Baralho;
+        Carta Manilha = new Carta();
 
         public void teste()
         {
+            Jogador1 = new Player();
+            Jogador2 = new Player();
+            Baralho = new Baralho();
+
+            Baralho.GerarBaralho();
+            Distribuir();
+            
         }
 
-        public void Rodada()
+        public void Distribuir()
         {
-            Jogador1.Carta1 = Carta.
+            Manilha = Baralho.GetTopCard();
+
+            Jogador1.Carta1 = Baralho.GetTopCard();
+            Jogador1.Carta2 = Baralho.GetTopCard();
+            Jogador1.Carta3 = Baralho.GetTopCard();
+
+            Jogador2.Carta1 = Baralho.GetTopCard();
+            Jogador2.Carta2 = Baralho.GetTopCard();
+            Jogador2.Carta3 = Baralho.GetTopCard();
         }
     }
 }
